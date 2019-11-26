9f80cf453eac3aabd200ca0eaf0e72c4539d3924
Luiz <pioneirogu@hotmail.com>
2019-11-25 21:02:19 -0300

    finalizado

--- detect.py
+++ detect.py
@@ -3,7 +3,7 @@ from imutils.object_detection import non_max_suppression
 import imutils
 import numpy as np
 from requests import Session
-# from signalr import Connection
+from signalr import Connection
 
 def GetContorno(color):
     mascara = cv2.inRange(hsv, color, upper)
@@ -13,56 +13,48 @@ def GetContorno(color):
     contornos, _ = cv2.findContours(bordas, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     return contornos
 
-# with Session() as session:
-#     connection = Connection("http://localhost:5000/signalr", session)
+with Session() as session:
+    connection = Connection("http://25.66.196.232:8088/signalr", session)
+    conn = connection.register_hub('step5')
+    connection.start()
+    #create error handler
+    def print_error(error):
+        print('error: ', error)
+    #process errors
+    connection.error += print_error
 
-#     conn = connection.register_hub('step5')
-
-#     connection.start()
-
-#     #create error handler
-#     def print_error(error):
-#         print('error: ', error)
-
-#     #process errors
-#     connection.error += print_error
-
-laranja = np.array([0, 98,120]) # laranja
+laranja = np.array([0, 105,120]) # laranja
 upper = np.array([15, 255, 255])
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+# camera = cv2.VideoCapture('http://192.168.44.109:4747/video')
 camera = cv2.VideoCapture(0)
 while True:
     frame = camera.read()[1]
-    #frame = cv2.flip(frame, 1)
     frame = imutils.resize(frame, width=min(400, frame.shape[1]))
     rects, wights = hog.detectMultiScale(
         frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
     
     rects = np.array([[x, y, x+w, y+h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.85)
-
+    
     for (xA, yA, xB, yB) in pick:
         print('pessoas')
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         contornos = GetContorno(laranja)
         for contorno in contornos:
             area = cv2.contourArea(contorno)
+            x ,y ,w ,h = cv2.boundingRect(contorno)
             if area > 1500:
-                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
                 print("[INFO] {} pessoas detectadas".format(len(pick)))
-                # with connection:
-                #     while True:
-                #         #post new message
-                #         conn.server.invoke('alert', 'Trabalhador na pista.')    
-                #         #wait a second before exit
-                #         connection.wait(1)
+                conn.server.invoke('alert', 'Trabalhador na pista.')   
                         
     cv2.imshow("Camera", frame)
 
-    key = cv2.waitKey(10)
+    key = cv2.waitKey(5)
     if key == 27:
         break
 
